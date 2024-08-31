bd497a0eb55236bc6ba1f12ec64a6b4848695391
jagriti2005 <154238507+jagriti2005@users.noreply.github.com>
2024-08-31 19:58:47 +0000

    dataTypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,21 @@
+const accountId=144567
+let accountEmail="jagritirai098@gmail.com"
+var accountPassword="1234567"
+accountCity="Jaipur"
+
+accountEmail="hhagjhdsi"
+accountPassword="12345"
+accountCity="Gorakhpur"
+let accountState;
+
+// accountId=123890
+console.log(accountId)
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+console.table([accountId,accountPassword,accountEmail,accountCity,accountState])
+
+/*
+Prefer not to use var 
+bacause of issue in block space and functional scope
+*/
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,2 @@
 console.log("hitesh")
+console.log(4+5)
