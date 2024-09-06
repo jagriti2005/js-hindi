e9974902933f5700da2fc21ec6b167686a4db61b
jagriti2005 <154238507+jagriti2005@users.noreply.github.com>
2024-09-06 21:28:08 +0000

    operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -14,15 +14,48 @@
 
 
 
-let loggedIn = 1
-let BooleanloggedIn = Boolean(loggedIn);
-console.log(BooleanloggedIn)
+// let loggedIn = 1
+// let BooleanloggedIn = Boolean(loggedIn);
+// console.log(BooleanloggedIn)
 
 //1 => true; 0 => false
 // "" => false
 // "jagriti" => true
 
-let someNumber = 33;
-let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33;
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// *********************** Opeartions ******************************
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**3);
+// console.log(2%4);
+
+
+// let str1="Jagriti"
+// let str2=" Rai"
+// let str3=str1+str2
+// console.log(str3);
+
+// console.log('1'+2)
+// console.log(1+'2')
+// console.log('1'+2+2)  //122
+// console.log(1+2+'2')  //32
+
+// console.log(+true);  //1
+// console.log(+'');  //0
+
+// let gameCounter = 100;
+// gameCounter++
+// console.log(gameCounter);  //101
+// ++gameCounter
+// console.log(gameCounter);  //101
