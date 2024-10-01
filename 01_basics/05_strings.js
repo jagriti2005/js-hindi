2c88b623756a74ade4a9aec137e0698eb0ff0381
jagriti2005 <154238507+jagriti2005@users.noreply.github.com>
2024-10-01 07:07:18 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,30 @@
+// const name = "Jagriti";
+// const repoCount = 2005;
+// console.log(name + repoCount + " value");
+
+// console.log(`Hello my name is ${name} and my repo is ${repoCount}`);
+
+// const gameName = new String('jagriti');
+
+// console.log(gameName[0]);
+// console.log(gameName.length);
+// console.log(gameName.__proto__);
+// console.log(gameName);
+
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCase());
+// console.log(gameName.charAt(4));
+// console.log(gameName.indexOf('t'));
+// console.log(gameName.substring(0,4));
+// console.log(gameName.slice(1,4));
+
+// const anotherString = "     Jagriti    ";
+// console.log(anotherString);
+// console.log(anotherString.trim());
+
+const newString = "jagriti45@gmail.com/%20raijagu";
+console.log(newString.replace('%20','-'));
+console.log(newString.includes('jagriti'));
+
+const playGame = new String('jagriti-rai-jagu');
+console.log(playGame.split('-'));
