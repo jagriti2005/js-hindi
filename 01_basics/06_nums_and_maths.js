0ea78934dcc6ed5ec78ac575e0decdfc189a04b7
jagriti2005 <154238507+jagriti2005@users.noreply.github.com>
2024-11-17 06:38:28 +0000

    num and math

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,35 @@
+// const score = 400;
+// console.log(score);
+
+// const balance = new Number(100);
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));  //100.00
+
+
+// const otherNumber = 123.8966
+// console.log(otherNumber.toFixed(3));       //123.897
+// console.log(otherNumber.toPrecision(4));    //123.9
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString());    //1,000,000
+// console.log(hundreds.toLocaleString('en-IN'));    //10,00,000
+
+//+++++++++++++++++++ Maths ++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));   // for upper value....ans-> 5
+// console.log(Math.floor(4.6));   // for lower value....ans-> 4
+// console.log(Math.min(4,6,7,8,9,0)); 
+// console.log(Math.max(4,6,7,8,9,0)); 
+
+
+console.log(Math.random());
+console.log(Math.random()*10 + 1);
+console.log(Math.random()*100 + 1);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
